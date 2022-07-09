4b7218366ebaa85c36f85b6b9954257232a96a5a
Vansh Shah <65898155+panthchristian@users.noreply.github.com>
2022-07-09 10:34:01 +0000

    Co-authored-by: Feitan Portor <FeitanPortor@users.noreply.github.com>

---
+++ epharma/my-project/pages/contact.js
@@ -0,0 +1,58 @@
+// import React from 'react'
+import Nav from '../components/Nav'
+
+export default function contact() {
+    return (
+        <>
+        <Nav />
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-12 mx-auto flex sm:flex-nowrap sm:flex-col flex-wrap">
+                <div class="h-[25rem] lg:w-2/3 md:w-1/2 sm:w-full bg-gray-300 rounded-lg overflow-hidden flex items-center justify-center">
+                    <iframe class="inset-0" frameborder="0" title="map" marginheight="0" marginwidth="0" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7736499.148433491!2d72.27657120898787!3d18.771954772859896!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcfc41e9c9cd6f9%3A0x1b2f22924be04fb6!2sMaharashtra%2C%20India!5e0!3m2!1sen!2sus!4v1657361232357!5m2!1sen!2sus" width="100%" height="100%" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    
+                </div>
+                <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <div class="bg-white flex flex-wrap py-6 rounded shadow-md">
+                        <div class="lg:w-1/2 px-6">
+                            <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+                            <p class="mt-1">Photo booth tattooed prism, portland taiyaki hoodie neutra typewriter</p>
+                        </div>
+                        <div class="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                            <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+                            <a class="text-indigo-500 leading-relaxed">example@email.com</a>
+                            <h2 class="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+                            <p class="leading-relaxed">123-456-7890</p>
+                        </div>
+                    </div>
+                    <div class="bg-white flex flex-wrap py-6 mt-4 rounded shadow-md">
+                        <div class="lg:w-1/2 px-6">
+                            <h2 class="title-font font-semibold text-gray-900 tracking-widest text-md w-64">Hours of Operation</h2>
+                            <p class="leading-relaxed w-64">Mon - Fri : 9 A.M. to 6 P.M.</p>
+                            <p class="leading-relaxed w-64">Sat : 9 A.M. to 2 P.M.</p>
+                            <p class="leading-relaxed">Sun : Closed</p>
+                        </div>
+                    </div>
+                    {/* <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Feedback</h2>
+                    <p class="leading-relaxed mb-5 text-gray-600">Post-ironic portland shabby chic echo park, banjo fashion axe</p>
+                    <div class="relative mb-4">
+                        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                        <input type="text" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                    </div>
+                    <div class="relative mb-4">
+                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    </div>
+                    <button class="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                    <p class="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p> */}
+                </div>
+            </div>
+        </section>
+        </>
+        
+    )
+}
+
